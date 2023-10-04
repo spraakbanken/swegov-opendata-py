bfbf127d98404a9b89747a7457685042f47ab99e
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-10-04 15:14:01 +0200

    style: after fmt

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,15 @@
 from swegov_opendata.preprocess import preprocess_corpora
 
-
 if __name__ == "__main__":
     # corpora = ["rd-bet", "rd-ds", "rd-eun", "rd-flista", "rd-fpm", "rd-frsrdg", "rd-ip",
     #            "rd-kammakt", "rd-kom", "rd-mot", "rd-ovr", "rd-prop", "rd-prot", "rd-rskr",
     #            "rd-samtr", "rd-skfr", "rd-sou", "rd-tlista", "rd-utr", "rd-utsk", "rd-yttr"]
 
-    preprocess_corpora(corpora=["rd-fpm"], skip_files=[],
-                     #  testfile="h7a2eun10p.xml"
-                       )
+    preprocess_corpora(
+        corpora=["rd-fpm"],
+        skip_files=[],
+        #  testfile="h7a2eun10p.xml"
+    )
 
     # with open("gr091.xml") as f:
     #     xml_string = f.read()
